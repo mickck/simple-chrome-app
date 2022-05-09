72058b58175d686ee8e601f669c2e647d0c42524
Mickck <mikemwkim@gmail.com>
2022-05-09 22:52:43 +1000

    made login form using localstorage and making a function paintGreetings

--- app.js
+++ app.js
@@ -1,26 +1,40 @@
 const loginForm = document.querySelector("#login-form");
-const loginInput = document.querySelector("#login-form input"); // find input of #login-form
+const loginInput = document.querySelector("#login-form input"); 
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME ="hidden";
+const USERNAME_KEY ="username";
 
 function onLoginSubmit(event){
     event.preventDefault();
     //default action that belongs to the event will not occur. this time when I dont'put preeventDefault and press submit, the website do refresh itselfts automatically.
-    const userName = loginInput.value;
-    localStorage.setItem("username",userName);
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY,username);
     // put username to "username"(key)
-    console.log(userName);
+    console.log(username);
 
     loginForm.classList.add(HIDDEN_CLASSNAME); 
     // add class hidden to #login-form it hide form after click a button
   console.log(loginForm);
   //check class hidden w/ loginForm
-  greeting.classList.remove(HIDDEN_CLASSNAME);
-  greeting.innerText = `Hello  +${userName} `;
-
- }
+  paintGreetings(username); //username comes from the login-form/input.value
+  }
  
+ function paintGreetings(username){
+  greeting.innerText = `Hello ${username} `;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+ }
+
  loginForm.addEventListener("submit",onLoginSubmit);
 
- 
+
+ const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+ if(savedUsername=== null){
+   // show the form
+   loginForm.classList.remove(HIDDEN_CLASSNAME); 
+   //loginForm.addEventListener("submit", onLoginSubmit);
+ }else{
+  paintGreetings(savedUsername);// username comes from the Localstorage
+  // show the greeting
+ }
