131ce673aa8ac05afe23d0f0024a6737b79d9f7f
Mickck <mikemwkim@gmail.com>
2022-05-10 23:20:37 +1000

    make clock using date, padStart(for 00) and setInterval

--- js/clock.js
+++ js/clock.js
@@ -1,10 +1,16 @@
 const clock = document.querySelector("h2#clock");
 
-clock.innerText ="how are you?"
 
-function sayHello(){
-    console.log("hello");
+
+function getClock(){
+    const date = new Date;
+    const hours =String( date.getHours()).padStart(2,"0");
+    const minutes = String(date.getMinutes()).padStart(2,"0");
+    const seconds = String(date.getSeconds()).padStart(2,"0");
+    clock.innerText =`${hours}:${minutes}:${seconds}`;
 }
-
-setInterval(sayHello, 5000);
+getClock();
+setInterval(getClock, 1000);
 // sayHello() every 5sec
+// setTimeout(expression, timeout); runs the code/function once after the timeout.
+// setInterval(expression, timeout); runs the code/function repeatedly, with the length of the timeout between each repeat.
