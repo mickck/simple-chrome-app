bf10a8d5fd7e4f93d96187f48f943ca417adca1a
Mickck <mikemwkim@gmail.com>
2022-05-21 19:40:13 +1000

    saving todos using  //JSON.stringify : make a string everything in parenthesis

--- js/todo.js
+++ js/todo.js
@@ -3,13 +3,28 @@ const toDoInput = toDoForm.querySelector("input");
 //same   const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+const toDos = [];
+
+function saveTodos(){
+    localStorage.setItem("todos",JSON.stringify(toDos));
+    //JSON.stringify : make a string everything in 
+}
+
+function deleteTodo(event){
+    const li =event.target.parentElement;
+    //event == value, event.taget == button, event.target.parentElement == button's li
+    li.remove();
+}
 
 function paintTodo(newTodo){
 const li = document.createElement("li");
 const span = document.createElement("span");
-
+const button  =document.createElement("button");
 li.appendChild(span);
+li.appendChild(button);
 span.innerText=newTodo;
+button.innerText="❌";
+button.addEventListener("click",deleteTodo);
 toDoList.appendChild(li);
 
 }
@@ -19,8 +34,9 @@ function handleToDoSubmit(event){
     const newTodo = toDoInput.value; 
     // put the todoInputs data to newTodo and delete words in the todoform; 
     toDoInput.value="";
+    toDos.push(newTodo);
     paintTodo(newTodo);
-    
+    saveTodos();
 };
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
